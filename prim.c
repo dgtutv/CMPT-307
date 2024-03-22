2369ff98f58fa67c0e782d433b5adcfa0e8bef35
Daniel Todd <dgtutv@gmail.com>
2024-03-22 13:14:31 -0700

    Changed approch to connecting our graph

--- prim.c
+++ prim.c
@@ -50,35 +50,25 @@ Graph* generateGraph(int n, int graphDensity){
     graph->numEdges = m;
     graph->edges = (Edge*)malloc(sizeof(Edge)*m);
 
-    //Make a connected graph (link all nodes in a cycle), as we initialize the nodes
-    int i;
-    for(i=0; i<n; i++){
-        //Node initialization
-        graph->nodes[i].index = i;
+    //Initialize our nodes
+    for(int i=0; i<n; i++){
+        graph->nodes[i].edges = (Edge*)malloc(sizeof(Edge)*2);
         graph->nodes[i].numEdges = 0;
-        graph->nodes[i].edges = (Edge*)malloc(sizeof(Edge)*2);   //All nodes in a connected graph have 2 edges
-
-        //Edge work
-        if(i>1){
-            graph->edges[i-1].first = graph->nodes[i-1];
-            graph->edges[i-1].second = graph->nodes[i];
-            graph->edges[i-1].weight = rand() % 30+1;
-            
-            //Update the edges list and edge count for each node
-            graph->nodes[i-1].edges[graph->nodes[i-1].numEdges-1] = graph->edges[i-1];
-            graph->nodes[i].edges[graph->nodes[i].numEdges-1] = graph->edges[i-1];
-            graph->nodes[i-1].numEdges++;
-            graph->nodes[i].numEdges++;
-        }        
+        graph->nodes[i].index = i;
     }
-    //Link the first and last edges
-    graph->edges[i].first = graph->nodes[0];
-    graph->edges[i].second = graph->nodes[i];
-    graph->edges[i].weight = rand() % 30+1;
-    graph->nodes[0].edges[1] = graph->edges[i];
-    graph->nodes[i-1].edges[1] = graph->edges[i];
-    graph->nodes[0].numEdges++;
-    graph->nodes[i].numEdges++;
+
+    //Make a connected graph (link all nodes in a cycle)
+    int j=1;
+    for(int i=0; j<n; i++){
+        graph->edges[i].weight = rand()%30+1;
+        graph->edges[i].first = graph->nodes[i];
+        graph->edges[i].second = graph->nodes[j];
+        j++;
+    }
+    //Link the first and last nodes
+    graph->edges[n-1].weight = rand()%30+1;
+    graph->edges[n-1].first = graph->nodes[n-1];
+    graph->edges[n-1].second = graph->nodes[0];
 
     //Add any remaining edges (later)
     return graph;

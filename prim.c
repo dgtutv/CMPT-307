ab14d64bdc3ffea2326c67b9481cd354c12c0a89
Daniel Todd <dgtutv@gmail.com>
2024-03-22 14:13:06 -0700

    Attempting to construct graph figure 1 from assignment, memory issues

--- prim.c
+++ prim.c
@@ -81,19 +81,100 @@ Graph* generateGraph(int n, int graphDensity){
 }
 
 int main(){
-    Graph* graph1 = generateGraph(20, 2);
-    printf("n: ");
-    printf("%d\n", graph1->numNodes);
-    printf("m: ");
-    printf("%d\n", graph1->numEdges);
-    printf("Nodes: ");
-    for(int i=0; i<graph1->numNodes; i++){
-        printf("%d ", graph1->nodes[i].index);
-    }
-    printf("\nEdges: ");
-    for(int i=0; i<graph1->numEdges; i++){
-        printf("(%d, %d)", graph1->edges[i].first.index, graph1->edges[i].second.index);
-    }
-    printf("\n");
+    //Generate the graph in figure 1, a-i = 0-8
+    Graph* fig1 = (Graph*)malloc(sizeof(Graph));
+    fig1->numNodes = 9;
+    fig1->numEdges = 14;
+    for(int i=0; i<fig1->numNodes; i++){    //Initialize our nodes
+        fig1->nodes[i].index = i;
+    }   
+    fig1->nodes[0].numEdges = 2;
+    fig1->edges[0].weight = 4;
+    fig1->edges[0].first = fig1->nodes[0];
+    fig1->edges[0].second = fig1->nodes[1];
+    fig1->nodes[0].edges = (Edge*)malloc(sizeof(Edge)*2);
+    fig1->nodes[0].edges[0] = fig1->edges[0];
+    fig1->edges[1].weight = 8;
+    fig1->edges[1].first = fig1->nodes[0];
+    fig1->edges[1].second = fig1->nodes[7];
+    fig1->nodes[0].edges[1] = fig1->edges[1];
+    fig1->nodes[1].numEdges = 3;
+    fig1->nodes[1].edges = (Edge*)malloc(sizeof(Edge)*3);
+    fig1->nodes[1].edges[0] = fig1->edges[0];
+    fig1->edges[2].weight = 8;
+    fig1->edges[2].first = fig1->nodes[1];
+    fig1->edges[2].second = fig1->nodes[2];
+    fig1->nodes[1].edges[1] = fig1->edges[2];
+    fig1->edges[3].weight = 11;
+    fig1->edges[3].first = fig1->nodes[1];
+    fig1->edges[3].second = fig1->nodes[7];
+    fig1->nodes[1].edges[2] = fig1->edges[3];
+    fig1->nodes[7].numEdges = 4;
+    fig1->nodes[7].edges = (Edge*)malloc(sizeof(Edge)*4);
+    fig1->nodes[7].edges[0] = fig1->edges[1];
+    fig1->nodes[7].edges[1] = fig1->edges[3];
+    fig1->edges[4].weight = 7;
+    fig1->edges[4].first = fig1->nodes[7];
+    fig1->edges[4].second = fig1->nodes[8];
+    fig1->nodes[7].edges[2] = fig1->edges[4];
+    fig1->edges[5].weight = 1;
+    fig1->edges[5].first = fig1->nodes[7];
+    fig1->edges[5].second = fig1->nodes[6];
+    fig1->nodes[7].edges[3] = fig1->edges[5];
+    fig1->nodes[8].numEdges = 3;
+    fig1->nodes[8].edges = (Edge*)malloc(sizeof(Edge)*3);
+    fig1->nodes[8].edges[0] = fig1->edges[4];
+    fig1->edges[6].weight = 2;
+    fig1->edges[6].first = fig1->nodes[8];
+    fig1->edges[6].second = fig1->nodes[2];
+    fig1->nodes[8].edges[1] = fig1->edges[6];
+    fig1->edges[7].weight = 6;
+    fig1->edges[7].first = fig1->nodes[8];
+    fig1->edges[7].second = fig1->nodes[6];
+    fig1->nodes[8].edges[2] = fig1->edges[7];
+    fig1->nodes[2].numEdges = 4;
+    fig1->nodes[2].edges = (Edge*)malloc(sizeof(Edge)*4);
+    fig1->nodes[2].edges[0] = fig1->edges[2];
+    fig1->nodes[2].edges[1] = fig1->edges[6];
+    fig1->edges[8].weight = 7;
+    fig1->edges[8].first = fig1->nodes[2];
+    fig1->edges[8].second = fig1->nodes[3];
+    fig1->nodes[2].edges[2] = fig1->edges[8];
+    fig1->edges[9].weight = 4;
+    fig1->edges[9].first = fig1->nodes[2];
+    fig1->edges[9].second = fig1->nodes[5];
+    fig1->nodes[2].edges[3] = fig1->edges[9];
+    fig1->nodes[6].numEdges = 3;
+    fig1->nodes[6].edges = (Edge*)malloc(sizeof(Edge)*3);
+    fig1->nodes[6].edges[0] = fig1->edges[5];
+    fig1->nodes[6].edges[1] = fig1->edges[7];
+    fig1->edges[10].weight = 2;
+    fig1->edges[10].first = fig1->nodes[6];
+    fig1->edges[10].second = fig1->nodes[5];
+    fig1->nodes[6].edges[2] = fig1->edges[10];
+    fig1->nodes[3].numEdges = 3;
+    fig1->nodes[3].edges = (Edge*)malloc(sizeof(Edge)*3);
+    fig1->nodes[3].edges[0] = fig1->edges[8];
+    fig1->edges[11].weight = 9;
+    fig1->edges[11].first = fig1->nodes[3];
+    fig1->edges[11].second = fig1->nodes[4];
+    fig1->nodes[3].edges[1] = fig1->edges[11];
+    fig1->edges[12].weight = 14;
+    fig1->edges[12].first = fig1->nodes[3];
+    fig1->edges[12].second = fig1->nodes[5];
+    fig1->nodes[3].edges[2] = fig1->edges[12];
+    fig1->nodes[4].numEdges = 2;
+    fig1->nodes[4].edges = (Edge*)malloc(sizeof(Edge)*2);
+    fig1->nodes[4].edges[0] = fig1->edges[11];
+    fig1->edges[13].weight = 10;
+    fig1->edges[13].first = fig1->nodes[4];
+    fig1->edges[13].second = fig1->nodes[5];
+    fig1->nodes[4].edges[1] = fig1->edges[13];
+    fig1->nodes[5].numEdges = 4;
+    fig1->nodes[5].edges = (Edge*)malloc(sizeof(Edge)*4);
+    fig1->nodes[5].edges[0] = fig1->edges[10];
+    fig1->nodes[5].edges[1] = fig1->edges[9];
+    fig1->nodes[5].edges[2] = fig1->edges[12];
+    fig1->nodes[5].edges[3] = fig1->edges[13];
     return 0;
 }

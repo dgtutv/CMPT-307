23ebde0b3702d8b32d9db025ce8205c8f338f908
Daniel Todd <dgtutv@gmail.com>
2024-04-03 14:31:31 -0700

    Working on testing Graph.generate for the base graph

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -6,18 +6,29 @@ using namespace std;
 class Node;
 
 class Edge{
+    public:
     vector<Node*> nodes;
+    int index;
+    int numNodes;
+    Edge(int index){
+        this->index = index;
+        this->numNodes = 0;
+    }
 };
 
 class Node{
+    public:
     vector<Edge*> edges;
     int numEdges;
-    Node(int numEdges){
-        this->numEdges = numEdges;
+    int index;
+    Node(int index){
+        this->index = index;
+        this->numEdges = 0;
     }
 };
 
 class Graph{
+    public:
     int numNodes;
     int numEdges;
     vector<Node*> nodes;
@@ -33,10 +44,53 @@ class Graph{
     }
 
     void generate(){
+        //Initialize all of our nodes
+        for(int i=0; i<numNodes; i++){
+            Node* currentNode = new Node(i);
+            nodes.push_back(currentNode);
+        }
+
         //Make a connected base graph of nodes
+        for(int i=0; i<numEdges; i++){
+            Edge* currentEdge = new Edge(i);
+            edges.push_back(currentEdge);
+            currentEdge->numNodes = 2;
+            currentEdge->nodes.push_back(nodes[i]);
+            nodes[i]->numEdges = 2;
+            nodes[i]->edges.push_back(currentEdge);
+            if(i < numEdges-1){
+                currentEdge->nodes.push_back(nodes[i+1]);
+                nodes[i+1]->edges.push_back(currentEdge);
+            }
+            else{   //If the last edge in making our connected graph, link to the first node
+                currentEdge->nodes.push_back(nodes[0]);
+                nodes[0]->edges.push_back(currentEdge);
+                edges[0]->nodes.push_back(nodes[i]);
+            }
+            
+        }
+    }
+
+    void print(){
+        cout << "numNodes: " << numNodes << endl;
+        cout << "numEdges: " << numEdges << endl;
+
+        //Print all of the nodes, with what edges are connected 
+        cout << "Nodes:" << endl;
+        for(Node* currentNode : nodes){
+            cout << "\tindex: " << currentNode->index << endl;
+            cout << "\tnumEdges: " << currentNode->numEdges << endl;
+            cout << "\tEdges: " << endl;
+            for(Edge* currentEdge : currentNode->edges){
+                cout << "\t\tindex: " << currentEdge->index << endl;
+            }
+        }
+
+        //Print all edges, with all the nodes that are connected
+
     }
 };
 
 void main(){
-
+    //Testing Graph.generate for the base graph
 }
